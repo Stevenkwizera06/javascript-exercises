bfbc54d1f603bf1f159ca67f3822aa12224e5010
unknown <kwiste06@gmail.com>
2023-04-05 12:03:30 +0200

    did some exercises

---
+++ index.js
@@ -0,0 +1,40 @@
+// Slice method
+/**
+ 
+let FullName = "steven kwizera";
+let LastName;
+let firstName;
+
+LastName = FullName.slice(0,6)
+firstName = FullName.slice(FullName.indexOf(" ") +1)
+
+console.log(LastName)
+console.log(firstName)
+
+ */
+
+
+// Method chaining
+
+/**
+ let userName = "Steven";
+let letter = userName.charAt(5).toUpperCase();
+console.log(letter);
+ */
+
+
+//If statements
+let age = 80;
+if(age >= 70){
+       console.log("your senior citizen")
+       }
+else if(age >= 18){
+console.log("you are  adult")
+}
+else if(age < 0){
+console.log("you are not yet born")
+}
+
+else{
+       console.log("you are not adult")
+}
